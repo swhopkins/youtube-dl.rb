229473ebfc6f0023613100dfc343ec5afc5199c6
Stephen Hopkins <swhopkins@gmail.com>
2020-10-06 12:08:18 -0400

    Upgrde to 2020.9.20

--- lib/youtube-dl/version.rb
+++ lib/youtube-dl/version.rb
@@ -6,5 +6,5 @@
 module YoutubeDL
   # Semantic Version as well as the bundled binary version.
   # "(major).(minor).(teeny).(pre-release).(binary-version)"
-  VERSION = '2020.09.06'.freeze
+  VERSION = '2020.09.20'.freeze
 end
