a8963c9be72bce0df0ae44360385cf9a807f96b3
Stephen Hopkins <swhopkins@gmail.com>
2022-09-26 16:04:24 -0400

    update to 2022.09.01

--- lib/youtube-dl/version.rb
+++ lib/youtube-dl/version.rb
@@ -6,5 +6,5 @@
 module YoutubeDL
   # Semantic Version as well as the bundled binary version.
   # "(major).(minor).(teeny).(pre-release).(binary-version)"
-  VERSION = '2022.08.19'.freeze
+  VERSION = '2022.09.01'.freeze
 end
