2236f3de4c5e52c39547f7636fc52391ac706663
Stephen Hopkins <swhopkins@gmail.com>
2023-08-09 13:47:11 -0400

    Update binary to 2023.07.06

--- lib/youtube-dl/version.rb
+++ lib/youtube-dl/version.rb
@@ -6,5 +6,5 @@
 module YoutubeDL
   # Semantic Version as well as the bundled binary version.
   # "(major).(minor).(teeny).(pre-release).(binary-version)"
-  VERSION = '2022.09.01'.freeze
+  VERSION = '2023.07.06'.freeze
 end
