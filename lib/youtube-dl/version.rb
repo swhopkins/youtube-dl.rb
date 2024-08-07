35b8ef8b52693b8f21bd9e489e6c54578e1dd8a6
Stephen Hopkins <swhopkins@gmail.com>
2024-08-07 09:51:44 -0400

    Upgrade to 2024.08.06

--- lib/youtube-dl/version.rb
+++ lib/youtube-dl/version.rb
@@ -6,5 +6,5 @@
 module YoutubeDL
   # Semantic Version as well as the bundled binary version.
   # "(major).(minor).(teeny).(pre-release).(binary-version)"
-  VERSION = '2024.07.07'.freeze
+  VERSION = '2024.08.06'.freeze
 end
